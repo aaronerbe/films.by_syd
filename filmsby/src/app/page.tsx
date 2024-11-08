5b535a8b4630978a3a74cd5e9172cbb7b1b8da87
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-11-07 23:32:47 -0700

    another vid on home page

--- filmsby/src/app/page.tsx
+++ filmsby/src/app/page.tsx
@@ -18,7 +18,7 @@ import {BackgroundVideo} from '@/app/components/BackgroundVideo'
 export default function Home() {
   return (
     <div className="absolute inset-0 h-screen w-screen bg-cover bg-center">
-      <BackgroundVideo videos={["/video/SamGarrett.mp4", "/video/TylerAddi.mp4"]} />
+      <BackgroundVideo videos={["/video/SamGarrett.mp4", "/video/TylerAddi.mp4", "/video/Aubrie.mp4"]} />
       <div className="flex items-center justify-center h-[300px]">
       <h1 className="font-alt text-4xl lg:text-5xl font-bold text-white text-center">Films By Syd</h1>
 
